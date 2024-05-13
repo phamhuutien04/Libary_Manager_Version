8aff9642a56ff781ae331bd8573b1f91b5d3390f
phamhuutien04 <tienreport808@gmail.com>
2024-05-13 20:50:24 +0700

    Edit

---
+++ Libary_BUS/BUS_DangNhap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libary_Manager.Libary_DAO;
+using Libary_Manager.Libary_DTO;
+using System.Windows.Forms;
+
+namespace Libary_Manager.Libary_BUS
+{
+    internal class BUS_DangNhap
+    {
+        private DAO_DangNhap dangNhapDAO;
+        public BUS_DangNhap()
+        {
+            this.dangNhapDAO = new DAO_DangNhap();
+        }
+
+        public bool checkDangNhap(DTO_DangNhap dangNhapDTO)
+        {
+            try
+            {
+                DataTable data = dangNhapDAO.checkDangNhap(dangNhapDTO);
+                return data != null;
+                MessageBox.Show("Đăng nhập thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                Controller.isAlert("Thông tin tài khoản hoặc mật khẩu không chính xác" + ex.Message, "Lỗi", System.Windows.Forms.MessageBoxIcon.Error);
+                return false;
+            }
+        }
+    }
+}
---
+++ Libary_DTO/DTO_DangNhap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libary_Manager.Libary_DTO
+{
+    internal class DTO_DangNhap
+    {
+        public int id { get; set; }
+        public string taiKhoan { get; set; }
+        public string matKhau { get; set; }
+        public int quyen { get; set; }
+        public DateTime ngayCap { get; set; }
+        public int maThe { get; set; }
+        public String hoTen { get; set; }
+        public DateTime ngaySinh { get; set; }
+        public String gioi { get; set; }
+    }
+}
